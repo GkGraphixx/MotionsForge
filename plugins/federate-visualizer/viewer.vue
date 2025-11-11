f061f4a1a8285b7d767425b1a31925b67bbc3acf
GkGraphixx <chinkeefloww@gmail.com>
2025-11-11 10:45:02 -0600

    Add viewer component for federate visualizer

---
+++ plugins/federate-visualizer/viewer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="viewer-container">
+    <canvas ref="canvas" class="mesh-canvas"></canvas>
+    <div class="collab-panel">
+      <textarea v-model="chat" placeholder="Type your message..."></textarea>
+      <button @click="sendMessage">Send</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { loadMesh } from './meshLoader.js';
+
+export default {
+  name: 'FederateVisualizer',
+  data() {
+    return {
+      canvas: null,
+      gl: null,
+      chat: ''
+    };
+  },
+  mounted() {
+    this.canvas = this.$refs.canvas;
+    this.gl = this.canvas.getContext('webgl');
+    loadMesh(this.gl);
+  },
+  methods: {
+    sendMessage() {
+      console.log('Collaborator says:', this.chat);
+      this.chat = '';
+    }
+  }
+};
+</script>
+
+<style scoped src="./styles.css"></style>

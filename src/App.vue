71580b93fe6d7a1ece2b985369523776b871f876
GkGraphixx <chinkeefloww@gmail.com>
2025-11-11 11:42:51 -0600

    Create App.vue for the main application layout

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>MotionsForge Plugin System</h1>
+    <!-- Plugins will mount here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  background-color: #121212;
+  color: #fff;
+  font-family: 'Segoe UI', sans-serif;
+}
+</style>

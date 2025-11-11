29b041f4c66141c25789ab35f5d48ae7cc673c42
GkGraphixx <chinkeefloww@gmail.com>
2025-11-11 10:57:14 -0600

    Add MonacoEditorOverlay component with installation method

--- plugins/monaco-overlay/index.js
+++ plugins/monaco-overlay/index.js
@@ -1,1 +1,10 @@
+import MonacoOverlay from './overlay.vue';
+
+export default {
+  name: 'MonacoEditorOverlay',
+  component: MonacoOverlay,
+  install(app) {
+    app.component('MonacoEditorOverlay', MonacoOverlay);
+  }
+};
 

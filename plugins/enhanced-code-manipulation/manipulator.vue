cffc10dd68bf135956067a5c37dc13ee14bfee8b
GkGraphixx <chinkeefloww@gmail.com>
2025-11-11 10:42:07 -0600

    Add EnhancedCodeManipulator component with editor

---
+++ plugins/enhanced-code-manipulation/manipulator.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="manipulator-container">
+    <div id="editor" class="monaco-editor"></div>
+    <div class="controls">
+      <button @click="regeneratePanel">Regenerate Panel</button>
+      <button @click="animateExport">Animate Export</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as monaco from 'monaco-editor';
+
+export default {
+  name: 'EnhancedCodeManipulator',
+  data() {
+    return {
+      editor: null,
+      defaultCode: `G1 X10 Y10\nG1 X20 Y20\nG1 X30 Y10\nG1 X40 Y30`
+    };
+  },
+  mounted() {
+    this.editor = monaco.editor.create(document.getElementById('editor'), {
+      value: this.defaultCode,
+      language: 'plaintext',
+      theme: 'vs-dark',
+      automaticLayout: true
+    });
+  },
+  methods: {
+    regeneratePanel() {
+      const code = this.editor.getValue();
+      console.log('Regenerating panel with code:', code);
+      // Add logic to recompile or reparse G-code
+    },
+    animateExport() {
+      const code = this.editor.getValue();
+      console.log('Animating export to SD/local with code:', code);
+      // Add animation or export logic here
+    }
+  }
+};
+</script>
+
+<style scoped src="./styles.css"></style>

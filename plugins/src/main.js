ee9d1a2a239a0cd5310bd6e1fed87f9e0076ceb7
GkGraphixx <chinkeefloww@gmail.com>
2025-11-11 11:09:48 -0600

    Delete plugins/src directory

--- plugins/src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import { registerAllPlugins } from './scripts/register-plugins.js';
-
-const app = createApp(App);
-registerAllPlugins(app);
-app.mount('#app');
